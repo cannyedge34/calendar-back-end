02620b26ae8f87dfb75628d7a6796cb64b265995
industriasarse <industriasarse@gmail.com>
2018-11-21 21:23:41 +0100

    Delete unnecessary id and rubocop offense

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,6 +1,6 @@
+# attributes to be serialized
 class EventSerializer < ActiveModel::Serializer
-  # attributes to be serialized
-  attributes :id, :title, :description
+  attributes :title, :description
   attribute :start_date, key: :start
   attribute :end_date, key: :end
 end
